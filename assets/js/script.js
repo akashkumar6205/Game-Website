6fb779f1853de418a457a5782c96babab12beb9d
akashkumar6205 <akashkumar23012005@gmail.com>
2026-06-06 16:02:57 +0530

    Add newsletter , footer section and back-to-top button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,16 +32,21 @@ for(let i = 0; i < searchTogglers.length; i++){
     });
 }
 
+
+
 /**
  * header
  */
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 window.addEventListener("scroll", function () {
     if(window.scrollY >= 200) {
         header.classList.add("active");
+        backTopBtn.classList.add("active");
     } else {
         header.classList.remove("active");
+        backTopBtn.classList.remove("active");
     }
 });
